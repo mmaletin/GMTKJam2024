277bf80df3e49d97d2a1311d8d87ac52e3ccb9fd
Maletin <maletinme@gmail.com>
2024-08-18 15:28:34 +0400

    Add grates

--- Assets/Scripts/FloorButton.cs
+++ Assets/Scripts/FloorButton.cs
@@ -1,20 +1,37 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class FloorButton : MonoBehaviour
 {
-    public bool isOn;
     public UnityEventBool onButtonStateChanged;
+
+    public SpriteRenderer spriteRenderer;
+    public Sprite normalSprite;
+    public Sprite pressedSprite;
+
+    private HashSet<Rigidbody2D> _pressingRigidbodies = new();
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //var cat = collision.attachedRigidbody.gameObject.GetComponent<CatController>();
-        //if (cat != null)
-        //{
-        //    isOn = !isOn;
-        //    onButtonStateChanged?.Invoke(isOn);
-        //}
+        var wasPressed = _pressingRigidbodies.Count > 0;
 
-        isOn = !isOn;
-        onButtonStateChanged?.Invoke(isOn);
+        _pressingRigidbodies.Add(collision.attachedRigidbody);
+        UpdateSprite();
+
+        if (!wasPressed)
+            onButtonStateChanged.Invoke(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _pressingRigidbodies.Remove(collision.attachedRigidbody);
+        UpdateSprite();
+        if (_pressingRigidbodies.Count == 0)
+            onButtonStateChanged.Invoke(false);
+    }
+
+    private void UpdateSprite()
+    {
+        spriteRenderer.sprite = _pressingRigidbodies.Count == 0 ? normalSprite : pressedSprite;
     }
 }
